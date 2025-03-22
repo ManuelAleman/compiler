1b4208b19cad12cfabf17d4e6c981918762c2e09
ManuelAleman <manuelitoaleman230@icloud.com>
2025-03-21 20:16:46 -0700

    IF BLOCK

--- src/com/compiler/MVC/model/LowLevel.java
+++ src/com/compiler/MVC/model/LowLevel.java
@@ -10,146 +10,194 @@ import java.util.List;
 
 public class LowLevel {
     private StringBuilder lowLevelCode;
+    private StringBuilder auxProcedures;
     private List<Variable> variables;
-
     private List<Simbol> tokens;
     private Simbol currentToken;
     private int index;
     private int labelCounter = 0;
 
-    private void next() {
-        index++;
-        currentToken = index < tokens.size() ? tokens.get(index) : null;
-    }
-    private boolean match(Token expect) {
-        return currentToken != null && currentToken.getTokenType() == expect;
-    }
-    public LowLevel(){
+    public LowLevel() {
         variables = new ArrayList<>();
         tokens = new ArrayList<>();
         lowLevelCode = new StringBuilder();
+        auxProcedures = new StringBuilder();
     }
+
     public void prepareLowLevel(List<Variable> variables, List<Simbol> tokens) {
         this.variables = variables;
         this.tokens = tokens;
         index = 0;
         currentToken = tokens.isEmpty() ? null : tokens.get(index);
+        lowLevelCode = new StringBuilder();
+        auxProcedures = new StringBuilder();
+        labelCounter = 0;
     }
 
-    public void analizeLowLevel(){
+    public void analizeLowLevel() {
+        lowLevelCode.append(".model small\n")
+                .append(".stack 100h\n");
+
         generateDataSegment();
         generateCodeSegment();
     }
 
-    private void generateDataSegment(){
+    private void generateDataSegment() {
         lowLevelCode.append(".DATA\n");
-        for (Variable variable : variables) {
-            lowLevelCode.append(LowLevelTemplate.dataTemplate(variable));
+        variables.forEach(variable ->
+                lowLevelCode.append(LowLevelTemplate.dataTemplate(variable))
+        );
+    }
+
+    private void generateCodeSegment() {
+        lowLevelCode.append(".CODE\n")
+                .append("main:\n")
+                .append(LowLevelTemplate.movTemplate("AX", "@DATA"))
+                .append(LowLevelTemplate.movTemplate("DS", "AX")).append("\n");
+
+        while (currentToken != null) {
+            processToken();
+            next();
+        }
+
+        lowLevelCode.append(auxProcedures)
+                .append(LowLevelTemplate.endOfProgram())
+                .append("\nEND main");
+    }
+
+    private void processToken() {
+        if (match(Token.LEFT_PAREN) || match(Token.RIGHT_PAREN)) {
+            next();
+            return;
+        }
+        if (match(Token.RW)) {
+            String value = currentToken.getValue();
+            if (isType(value)) {
+                consumeLine();
+            } else if ("if".equals(value)) {
+                generateIfTree();
+            } else if ("print".equals(value)) {
+                next();
+                next();
+                lowLevelCode.append(LowLevelTemplate.printTemplate(currentToken.getValue()));
+            } else if ("ENDE".equals(value)) {
+                lowLevelCode.append("   JMP ENDE0\n");
+            }
+        } else if (match(Token.IDENTIFIER)) {
+            processAssignment();
         }
     }
 
-    private void generateCodeSegment(){
-        lowLevelCode.append(".CODE\n");
-        while(currentToken != null){
-            if(match(Token.LEFT_PAREN) || match(Token.RIGHT_PAREN)){
-                next();
-                continue;
-            }
-            if(match(Token.RW) && isType(currentToken.getValue())){
-                consumeLine();
-            }
-            if(match(Token.IDENTIFIER)){
-                String varName = currentToken.getValue();
-                next();
-                if(match(Token.ASSIGN)){
-                    next();
-                    if(match(Token.FRACTION)){
-                        lowLevelCode.append(LowLevelTemplate.movTemplate(varName, currentToken.getValue()));
-                    }
-                    if(match(Token.NUMBER)){
-                        lowLevelCode.append(LowLevelTemplate.movTemplate(varName, currentToken.getValue()));
-                    }
-                    if(match(Token.STRING_VALUE)){
-                        lowLevelCode.append(LowLevelTemplate.stringAssigmentTemplate(varName, currentToken.getValue()));
-                    }
-                    next();
-                }
-            }
-            if(match(Token.RW) && "if".equals(currentToken.getValue())){
-                generateIfTree();
+    private void processAssignment() {
+        String varName = currentToken.getValue();
+        next();
+        if (match(Token.ASSIGN)) {
+            next();
+            if (match(Token.FRACTION) || match(Token.NUMBER)) {
+                lowLevelCode.append(LowLevelTemplate.movTemplate(varName, currentToken.getValue())).append("\n");
+            } else if (match(Token.STRING_VALUE)) {
+                lowLevelCode.append(LowLevelTemplate.stringAssigmentTemplate(varName, currentToken.getValue())).append("\n");
             }
             next();
         }
     }
 
-    private void generateIfTree(){
+    private void generateIfTree() {
         next();
         next();
-        if(match(Token.IDENTIFIER)){
+
+        if (match(Token.IDENTIFIER)) {
             String varName = currentToken.getValue();
             next();
             String operator = currentToken.getValue();
             next();
-            if(match(Token.NUMBER) || match(Token.FRACTION)){
+
+            if (match(Token.NUMBER) || match(Token.FRACTION)) {
                 String value = currentToken.getValue();
-                lowLevelCode.append(LowLevelTemplate.movTemplate("AX", varName)).append("\n");
-                lowLevelCode.append(LowLevelTemplate.movTemplate("BX", value)).append("\n");
-                lowLevelCode.append("\tCMP AX, BX\n");
-                if(operator.equals("==")){
-                    lowLevelCode.append("\tJE ");
-                }
-                if(operator.equals("!=")){
-                    lowLevelCode.append("\tJNE ");
-                }
-                if(operator.equals(">")){
-                    lowLevelCode.append("\tJG ");
-                }
-                if(operator.equals("<")){
-                    lowLevelCode.append("\tJL ");
-                }
-                if(operator.equals(">=")){
-                    lowLevelCode.append("\tJGE ");
-                }
-                if(operator.equals("<=")){
-                    lowLevelCode.append("\tJLE ");
-                }
+                lowLevelCode.append(LowLevelTemplate.movTemplate("AX", varName))
+                        .append(LowLevelTemplate.movTemplate("BX", value))
+                        .append("\tCMP AX, BX\n");
+
+                appendConditionalJump(operator);
                 lowLevelCode.append("ELSE").append(labelCounter).append("\n");
+                lowLevelCode.append("\n");
                 next();
                 next();
-                if(match(Token.LEFT_BRACE)){
-                    consumeLine();
-                }
-                lowLevelCode.append("\tJMP END").append(labelCounter).append("\n");
-                lowLevelCode.append("ELSE").append(labelCounter).append(":\n");
-                if(match(Token.RW) && "else".equals(currentToken.getValue())){
-                    next();
-                    next();
-                    if(match(Token.LEFT_BRACE)){
-                        consumeLine();
-                    }
-                }
-                lowLevelCode.append("END").append(labelCounter).append(":\n");
+                generateBlock("ELSE");
+                lowLevelCode.append("\n");
                 labelCounter++;
             }
-
         }
     }
 
-    private void consumeLine(){
-        while (currentToken.getTokenType() != Token.SEMICOLON){
+    private void generateBlock(String procedureName) {
+        auxProcedures.append(procedureName).append(labelCounter).append(":\n");
+        while (currentToken != null && !match(Token.RIGHT_BRACE)) {
+            if (match(Token.IDENTIFIER)) {
+                processAssignment();
+            } else if (match(Token.RW) && "if".equals(currentToken.getValue())) {
+                generateIfTree();
+            } else if (match(Token.RW) && "print".equals(currentToken.getValue())) {
+                next();
+                next();
+                auxProcedures.append(LowLevelTemplate.printTemplate(currentToken.getValue()));
+            }
             next();
         }
         next();
     }
+
+    private void next() {
+        index++;
+        currentToken = index < tokens.size() ? tokens.get(index) : null;
+    }
+
+    private boolean match(Token expect) {
+        return currentToken != null && currentToken.getTokenType() == expect;
+    }
+
+    private void consumeLine() {
+        while (currentToken != null && currentToken.getTokenType() != Token.SEMICOLON) {
+            next();
+        }
+
+    }
+
     private boolean isType(String value) {
         return "int".equals(value) || "double".equals(value) || "string".equals(value);
+    }
+
+    private void appendConditionalJump(String operator) {
+        switch (operator) {
+            case "==":
+                lowLevelCode.append("\tJE ");
+                break;
+            case "<>":
+                lowLevelCode.append("\tJNE ");
+                break;
+            case ">":
+                lowLevelCode.append("\tJG ");
+                break;
+            case "<":
+                lowLevelCode.append("\tJL ");
+                break;
+            case ">=":
+                lowLevelCode.append("\tJGE ");
+                break;
+            case "<=":
+                lowLevelCode.append("\tJLE ");
+                break;
+            default:
+                throw new IllegalArgumentException("Operador no soportado: " + operator);
+        }
+
     }
 
     public String getLowLevelCode() {
         return lowLevelCode.toString();
     }
 
-    public void clearLowLevel(){
+    public void clearLowLevel() {
         lowLevelCode = new StringBuilder();
     }
 }
--- src/com/compiler/utils/LowLevelTemplate.java
+++ src/com/compiler/utils/LowLevelTemplate.java
@@ -8,26 +8,36 @@ public class LowLevelTemplate {
             default -> "\t" + variable.getName() + " db 256 DUP(\"$\")\n";
         };
     }
+
     public static String printTemplate(String variable) {
         return "\tMOV AH, 09h\n" +
                 "\tLEA DX, " + variable + "\n" +
-                "\tINT 21h\n";
+                "\tINT 21h\n" + "\n";
     }
+
     public static String movTemplate(String destination, String source) {
         return "\tMOV " + destination + ", " + source + "\n";
     }
+
     public static String addTemplate(String destination, String source) {
         return "\tADD " + destination + ", " + source + "\n";
     }
-    public static String mulTemplate(String destination, String source) {
-        return "\tMUL " + destination + ", " + source + "\n";
-    }
+
     public static String subTemplate(String destination, String source) {
         return "\tSUB " + destination + ", " + source + "\n";
     }
-    public static String divTemplate(String destination, String source) {
-        return "\tDIV " + destination + ", " + source + "\n";
+
+    public static String mulTemplate(String operand) {
+        return "\tMOV AX, " + operand + "\n" +
+                "\tMUL BX\n";
     }
+
+    public static String divTemplate(String operand) {
+        return "\tMOV AX, " + operand + "\n" +
+                "\tMOV DX, 0\n" +
+                "\tDIV BX\n";
+    }
+
 
     public static String stringAssigmentTemplate(String varName, String stringValue) {
         StringBuilder stringCode = new StringBuilder();
@@ -40,4 +50,21 @@ public class LowLevelTemplate {
         return stringCode.toString();
     }
 
+    public static String printStringTemplate(String varName) {
+        return "	MOV	 DX, OFFSET " + varName + "\n" +
+                "	MOV	 AH, 09h\n" +
+                "	INT	 21h\n";
+    }
+
+    public static String prinIdTemplate(String varName) {
+        return "\tMOV AH, 09h\n" +
+                "\tLEA DX, " + varName + "\n" +
+                "\tINT 21h\n";
+    }
+
+    public static String endOfProgram() {
+        return  "ENDE0" + ":" + "\n" +
+                "    MOV AH, 4Ch" + "\n" +
+                "    INT 21h\n";
+    }
 }
